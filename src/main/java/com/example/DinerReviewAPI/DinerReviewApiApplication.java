327cc160d8425eaa1aa0a54422204ed0edcc7f39
VincentTrahin <vincent.trahin@gmail.com>
2024-02-17 08:23:54 +0100

    :tada: Initial commit: Set up Spring Boot project. Generated the initial Java project using Spring Initializr.

---
+++ src/main/java/com/example/DinerReviewAPI/DinerReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.DinerReviewAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinerReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinerReviewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DinerReviewAPI/DinerReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DinerReviewAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinerReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
